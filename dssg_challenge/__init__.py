7f7e937cd8e3ee06965aa1e40d12cfa6fb38346f
joaopfonseca <jpmrfonseca@gmail.com>
2020-10-10 11:52:53 +0100

    Adapted structure

---
+++ dssg_challenge/__init__.py
@@ -0,0 +1,10 @@
+from .compute_cost import compute_cost
+from .utils import check_keyboard, compute_cell_location
+from .draw_keyboard import draw
+
+__all__ = [
+    'compute_cost',
+    'check_keyboard',
+    'compute_cell_location',
+    'draw'
+]
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dssg_challenge',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code for the DSSG Summit 2020 Challenge: Keyboard layout optimization for ALS patients',
+    author='Joao Fonseca & David Silva',
+    license='MIT',
+)
