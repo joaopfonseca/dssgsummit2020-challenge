6ba3608c95b24abe4ef69de66265c6de30b79928
joaopfonseca <jpmrfonseca@gmail.com>
2020-10-10 11:07:47 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code for the DSSG Summit 2020 Challenge: Keyboard layout optimization for ALS patients',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
