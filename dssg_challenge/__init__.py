aab56b131823dce060e7d52418ccb2de15f5e40e
joaopfonseca <jpmrfonseca@gmail.com>
2021-01-08 00:16:36 +0000

    Added final results

--- dssg_challenge/__init__.py
+++ dssg_challenge/__init__.py
@@ -1,11 +1,13 @@
 from .compute_cost import compute_cost, compute_cost_ours
 from .utils import check_keyboard, compute_cell_location
 from .draw_keyboard import draw
+from ._concat_logs import concat_results
 
 __all__ = [
     'compute_cost',
     'compute_cost_ours',
     'check_keyboard',
     'compute_cell_location',
-    'draw'
+    'draw',
+    'concat_results'
 ]
